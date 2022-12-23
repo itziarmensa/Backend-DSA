12ad1b437043ed9eb750a5bf6f19b13cdd0e1ea0
Itziar Mensa <itziar.mensa@estudiantat.upc.edu>
2022-12-23 12:11:40 +0100

    Método comprar cambiado, ahora se hace a partir de un href

---
+++ public/js/tienda.js
@@ -0,0 +1,48 @@
+function comprar(idObjeto) {
+    $.ajax({
+        contentType: 'application/json',
+        type: 'PUT',
+        url: 'dsaApp/gameManager/user/' + sessionStorage.getItem("mailUsuario") + '/' + idObjeto,
+        dataType: 'text',
+        success: function (result) {
+            alert("Object bought successfully");
+        }
+    });
+
+}
+
+
+$(document).ready(function () {
+
+    if(sessionStorage.getItem('mailUsuario') != null){
+        $('#back').click(function () {
+            window.location.href='usuario.html';
+        });
+
+        $.ajax({
+            type: 'GET',
+            url: 'dsaApp/gameManager/myObjects',
+            dataType: 'text',
+            success: function (data) {
+                var json = $.parseJSON(data);
+                $('#results').empty();
+                for (var i = 0; i < json.length; ++i) {
+                    $('#results').append('<tr id="elementos">' +
+                        '<td>' + json[i].objectName + '</td>' +
+                        '<td>' + json[i].objectDescription + '</td>' +
+                        '<td>' + json[i].objectCoins + '</td>' +
+                        '<td>' + json[i].objectTypeId + '</td>' +
+                        '<td><a href="#" onclick="comprar('+json[i].objectId+')">Comprar</a></td>' +
+                        '</tr>');
+
+                }
+
+            },
+            error: function (error) {
+                alert("Not able to create the table. Try again");
+            }
+        });
+    }else {
+        window.location.href="index.html";
+    }
+});
